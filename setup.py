3d38c26794983416ad2acbbe9c3e7ed2e32a5389
Robert Collins <robertc@robertcollins.net>
2009-07-17 22:20:21 +1000

    Release 0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name="testresources",
-      version="0.1",
+      version="0.2",
       description="Testresources, a pyunit extension for managing expensive "
                   "test resources",
-      maintainer="Robert Collins",
-      maintainer_email="robertc@robertcollins.net",
-      url="http://www.robertcollins.net/unittest/testresources",
+      maintainer="Test Resources developers",
+      maintainer_email="https://launchpad.net/~testresources-developers",
+      url="https://launchpad.net/testresources",
       packages=['testresources', 'testresources.tests'],
       package_dir = {'':'lib'}
       )
